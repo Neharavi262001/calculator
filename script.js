45b2cbe9924af4bee19b8a4251eb5153cfd1904e
Neharavi262001 <neharavi262001@gmail.com>
2023-07-14 20:02:55 +0530

    calculator

---
+++ script.js
@@ -0,0 +1,16 @@
+function appendNumber(num) {
+    document.getElementById('result').value += num;
+}
+
+function appendOperator(operator) {
+    document.getElementById('result').value += operator;
+}
+
+function calculate() {
+    var result = eval(document.getElementById('result').value);
+    document.getElementById('result').value = result;
+}
+
+function clearResult() {
+    document.getElementById('result').value = '';
+}
